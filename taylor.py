2196d516b52a735fc30b40539b301e43f0f52cce
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2024-07-14 21:46:14 +0900

    change scales

--- taylor.py
+++ taylor.py
@@ -14,8 +14,8 @@ class TaylorSeriesSin(Scene):
 
         # Set up the axes
         axes = Axes(
-            x_range=[-3 * math.pi, 3 * math.pi, math.pi],  # x軸の範囲を-3πから3πに設定
-            y_range=[-1.5, 1.5, 0.5],
+            x_range=[-2 * math.pi, 2 * math.pi, math.pi],  # x軸の範囲を-3πから3πに設定
+            y_range=[-4, 4, 1],  # y軸の範囲を調整
             axis_config={"color": BLUE}
         )
 
